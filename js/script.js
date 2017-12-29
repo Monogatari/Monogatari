aeca01828ef1cf0dde955aec2e8efb6a5d2d18cc
Diego Islas Ocampo <hyuchia@gmail.com>
2017-12-29 12:30:19 -0600

    Remove extra trailing slash

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ var messages = {
 	"Help": {
 		"Title": "Help",
 		"Subtitle": "Some useful Links",
-		"Message": "<p><a href='https://monogatari.io/documentation//'>Documentation</a> - Everything you need to know.</p><p><a href='https://monogatari.io/demo/'>Demo</a> - A simple Demo.</p>"
+		"Message": "<p><a href='https://monogatari.io/documentation/'>Documentation</a> - Everything you need to know.</p><p><a href='https://monogatari.io/demo/'>Demo</a> - A simple Demo.</p>"
 	}
 };
 
