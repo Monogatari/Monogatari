e58737534eb02395e980cb4c662c34af5d50082e
Stephen Lavelle <analytic@gmail.com>
2018-07-21 05:14:21 +0200

    german translation
    I'm a very imperfect german speaker, but it should be comprehensible

---
+++ core/lib/translations/Deutsch.js
@@ -0,0 +1,77 @@
+/**
+ * ============================================================
+ * English
+ * ============================================================
+ *
+ * Translators:
+ *
+ * increpare <analytic@gmail.com>
+ */
+
+import { Monogatari } from './../monogatari';
+
+Monogatari.translation ('Deutsch', {
+
+	'AdvanceHelp': 'Um weiter durch das Spiel zu gehen, drucken die Leertaste oder klicken.',
+	'Audio': 'Audio',
+	'AutoPlay': 'Automatische Wiedergabe',
+	'AutoPlaySpeed': 'Wiedergabe Geschwindigkeit',
+
+	'Back': 'Zurück',
+	'BackButton': 'Zurück.',
+
+	'Cancel': 'Abbrechen',
+	'Close': 'Schließen',
+	'Confirm': 'Wollen Sie beenden?',
+
+	'Delete': 'Löschen',
+
+	'FullScreen': 'Vollbild',
+
+	'Help': 'Hilfe',
+	'Hide': 'Ausblenden',
+	'HideButton': 'Blenden Sie den Text aus.',
+
+	'iOSAudioWarning': 'Audio Einstellungen sind nicht von iOS unterstützt.',
+
+	'Language': 'Sprache',
+	'Load': 'Laden',
+	'LoadAutoSaveSlots': 'Automatisch gespeicherte Spiele',
+	'LoadButton': 'Offen dan Ladebildschirm.',
+	'Loading': 'Laden',
+	'LoadingMessage': 'Bitte warten, während des Ladens der Ressourcen.',
+	'LoadSlots': 'Gespeichertes Spiele',
+	'LocalStorageWarning': 'Dieser Browser ünterstützt Lokalen Speicher nicht.',
+
+	'Music': 'Musik Lautstärke',
+
+	'NoSavedGames': 'Keine gespeicherten Spiele.',
+	'NoAutoSavedGames': 'Keine automatisch gespeicherten Spiele',
+
+	'OrientationWarning': 'Um das Spiel zu spielen, Bitte drehen sie Ihr Gerät.',
+	'Overwrite': 'Überschreiben',
+
+	'QuickButtons': 'Schnellmenü Tasten',
+	'Quit': 'Beenden',
+	'QuitButton': 'Spiel Beenden.',
+
+	'Resolution': 'Auflösung',
+
+	'Save': 'Speichern',
+	'SaveButon': 'Offen den Speicherbildschirm.',
+	'SaveInSlot': 'Im Slot speichern',
+	'Settings': 'Einstellungen',
+	'SettingsButton': 'Einstellungen öffnen',
+	'Show': 'Einblenden',
+	'SlotDeletion': 'Sind Sie sicher, dass Sie dieses Slot löschen wollen?',
+	'SlotOverwrite': 'Sind Sie sicher, dass Sie dieses Slot überschreiben wollen?',
+	'Sound': 'Lautstärke',
+	'Start': 'Start',
+	'Stop': 'Stopp',
+
+	'TextSpeed': 'Text Geschwindigkeit',
+
+	'Voice': 'Stimme Lautstärke',
+
+	'Windowed': 'Fenstermodus'
+});
