b9e151c27b0d5c6604f214ea32acab95508d7178
Diego Islas Ocampo <dev@hyuchia.com>
2018-07-20 23:21:59 -0500

    Add initial approach for in-choice functions

--- core/lib/actions/Choice.js
+++ core/lib/actions/Choice.js
@@ -1,8 +1,13 @@
 import { Action } from '../Action';
 import { Monogatari } from '../monogatari';
-import { $_ } from '@aegis-framework/artemis';
+import { $_, Util } from '@aegis-framework/artemis';
 
 export class Choice extends Action {
+
+	static setup () {
+		Monogatari.global ('_CurrentChoice', null);
+		return Promise.resolve ();
+	}
 
 	static canProceed () {
 		if ($_(`${Monogatari.selector} [data-ui="choices"]`).isVisible ()) {
@@ -18,8 +23,19 @@ export class Choice extends Action {
 			if ($_(this).data('do') != 'null' && $_(this).data('do') != '') {
 				$_(`${selector} [data-ui="choices"]`).hide ();
 				$_(`${selector} [data-ui="choices"]`).html ('');
-				Monogatari.run ($_(this).data ('do'), false);
+				if (Monogatari.global ('_CurrentChoice') !== null) {
+					if (typeof Monogatari.global ('_CurrentChoice')[$_(this).data ('choice')] !== 'undefined') {
+						if (typeof Monogatari.global ('_CurrentChoice')[$_(this).data ('choice')].onChosen === 'function') {
+							Util.callAsync (Monogatari.global ('_CurrentChoice')[$_(this).data ('choice')].onChosen, Monogatari).then (() => {
+								Monogatari.run ($_(this).data ('do'), false);
+							});
+						}
+					}
+				} else {
+					Monogatari.run ($_(this).data ('do'), false);
+				}
 			}
+			Monogatari.global ('_CurrentChoice', null);
 		});
 		return Promise.resolve ();
 	}
@@ -27,6 +43,7 @@ export class Choice extends Action {
 	static reset () {
 		$_(`${Monogatari.selector} [data-ui="choices"]`).hide ();
 		$_(`${Monogatari.selector} [data-ui="choices"]`).html ('');
+		Monogatari.global ('_CurrentChoice', null);
 		return Promise.resolve ();
 	}
 
@@ -46,6 +63,7 @@ export class Choice extends Action {
 
 	apply () {
 
+		Monogatari.global ('_CurrentChoice', this.statement);
 		for (const i in this.statement) {
 			const choice = this.statement[i];
 
@@ -53,9 +71,9 @@ export class Choice extends Action {
 
 				Monogatari.assertAsync (this.statement[i].Condition, Monogatari).then (() => {
 					if (typeof choice.Class !== 'undefined') {
-						$_(`${Monogatari.selector} [data-ui="choices"]`).append (`<button data-do="${choice.Do}" class="${choice.Class}">${choice.Text}</button>`);
+						$_(`${Monogatari.selector} [data-ui="choices"]`).append (`<button data-do="${choice.Do}" class="${choice.Class}" data-choice="${i}">${choice.Text}</button>`);
 					} else {
-						$_(`${Monogatari.selector} [data-ui="choices"]`).append (`<button data-do="${choice.Do}">${choice.Text}</button>`);
+						$_(`${Monogatari.selector} [data-ui="choices"]`).append (`<button data-do="${choice.Do}" data-choice="${i}">${choice.Text}</button>`);
 					}
 				}).catch (() => {
 					// The condition wasn't met
@@ -65,9 +83,9 @@ export class Choice extends Action {
 			} else {
 				if (typeof choice == 'object') {
 					if (typeof choice.Class != 'undefined') {
-						$_(`${Monogatari.selector} [data-ui="choices"]`).append (`<button data-do="${choice.Do}" class="${choice.Class}">${choice.Text}</button>`);
+						$_(`${Monogatari.selector} [data-ui="choices"]`).append (`<button data-do="${choice.Do}" class="${choice.Class}" data-choice="${i}">${choice.Text}</button>`);
 					} else {
-						$_(`${Monogatari.selector} [data-ui="choices"]`).append (`<button data-do="${choice.Do}">${choice.Text}</button>`);
+						$_(`${Monogatari.selector} [data-ui="choices"]`).append (`<button data-do="${choice.Do}" data-choice="${i}">${choice.Text}</button>`);
 					}
 				} else if (typeof choice == 'string') {
 					Monogatari.run (choice, false);
