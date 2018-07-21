ea968ef662da26b24b52e7f642d47f164447ab00
Amaimersion <amatsukine@gmail.com>
2018-07-21 20:52:16 +0300

    Add russian translation

---
+++ core/lib/translations/Russian.js
@@ -0,0 +1,80 @@
+/**
+ * ============================================================
+ * Russian
+ * ============================================================
+ *
+ * Translators:
+ * Sergey Kuznetsov <https://github.com/Amaimersion>
+ */
+
+import { Monogatari } from './../monogatari';
+
+Monogatari.translation('Русский', {
+
+	/* Main menu. */
+
+	'Start': 'Начать игру', // Game menu.
+	'Load': 'Загрузить', // Load menu.
+	'Settings': 'Настройки', // Settings menu.
+	'Help': 'Помощь', // Help menu.
+
+	/* Game menu. */
+
+	'Back': 'Назад',
+	'Hide': 'Скрыть',
+	'Show': 'Показать',
+	'AutoPlay': 'Авто',
+	'Stop': 'Стоп',
+	'Save': 'Сохранить',
+	'Quit': 'Выйти',
+	'Confirm': 'Вы действительно хотите выйти?',
+
+	/* Load menu. */
+
+	'LoadSlots': 'Сохранённые игры',
+	'NoSavedGames': 'Нет сохранённых игр.',
+	'LoadAutoSaveSlots': 'Автосохранённые игры',
+	'NoAutoSavedGames': 'Нет автосохранённых игр.',
+
+	/* Settings menu. */
+
+	'Audio': 'Звук',
+	'Music': 'Громкость музыки',
+	'Sound': 'Громкость звука',
+	'Voice': 'Громкость голоса',
+	'TextSpeed': 'Скорость текста',
+	'AutoPlaySpeed': 'Скорость авточтения',
+
+	/* Help menu. */
+
+	'AdvanceHelp': 'Чтобы играть, нажимайте на пробел или на левую кнопку мыши.',
+	'QuickButtons': 'Кнопки быстрого меню',
+	'BackButton': 'Вернуться назад',
+	'HideButton': 'Скрыть текст',
+	'SaveButon': 'Открыть меню сохранения',
+	'LoadButton': 'Открыть меню загрузки',
+	'SettingsButton': 'Открыть меню настроек',
+	'QuitButton': 'Выйти из игры',
+
+	/* Buttons. */
+
+	'Close': 'Закрыть',
+	'Cancel': 'Отмена',
+	'Delete': 'Удалить',
+	'FullScreen': 'Полный экран',
+	'Language': 'Язык',
+	'Overwrite': 'Перезаписать',
+
+	/* Labels. */
+
+	'iOSAudioWarning': 'Настройки звука не поддерживаются на iOS.',
+	'Loading': 'Загрузка',
+	'LoadingMessage': 'Подождите, пожалуйста, игровые файлы загружаются',
+	'LocalStorageWarning': 'Локальное хранилище не доступно в этом браузере.',
+	'OrientationWarning': 'Чтобы играть, пожалуйста, поверните Ваше устройство',
+	'Resolution': 'Разрешение',
+	'SaveInSlot': 'Сохранить в слот',
+	'SlotDeletion': 'Вы действительно хотите удалить этот слот?',
+	'SlotOverwrite': 'Вы действительно хотите перезаписать этот слот?',
+	'Windowed': 'Оконный режим'
+});
