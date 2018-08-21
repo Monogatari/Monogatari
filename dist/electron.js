8d743904058e3d6077cc7d89550895b776faad4d
Diego Islas Ocampo <dev@hyuchia.com>
2018-08-20 21:23:49 -0500

    Update meta files

--- dist/electron.js
+++ dist/electron.js
@@ -46,7 +46,7 @@ function createWindow () {
 		fullscrenable: true,
 
 		// Set an icon for the window
-		icon: __dirname + '/img/icons/icon_128x128.png'
+		icon: __dirname + '/assets/icons/icon_128x128.png'
 	});
 
 	// and load the index.html of the app.
