2e063d205229b323c5a8fb5276934e1efd20ca2e
Diego Islas Ocampo <dev@hyuchia.com>
2018-08-21 21:32:04 -0500

    Update service worker

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,13 +1,15 @@
 'use strict';
 
 // The name of your game, no spaces or special characters.
-var name = 'Monogatari';
+const name = 'Monogatari';
 
 // The version of the cache, changing this will force everything to be cached
 // again.
-var version = '0.1.0';
+const version = '0.2.0';
 
-var files = [
+const files = [
+
+	'/',
 
 	// General Files
 	'manifest.json',
@@ -17,21 +19,16 @@ var files = [
 	'engine/monogatari.js',
 
 	// HTML Files
-	// 'index.html',
+	'index.html',
 
 	// Style Sheets
-
-	// 'style/main.css',
+	'style/main.css',
 
 	// JavaScript Files
-	'js/vendor/jquery.min.js',
-
-	// 'js/options.js',
-	// 'js/storage.js',
-	// 'js/script.js',
-	// 'js/main.js'
-
-	// Fonts
+	'js/options.js',
+	'js/storage.js',
+	'js/script.js',
+	'js/main.js',
 
 	// App Images
 	'favicon.ico',
@@ -40,92 +37,73 @@ var files = [
 	'assets/icons/icon_70x70.png',
 	'assets/icons/icon_76x76.png',
 	'assets/icons/icon_96x96.png',
+	'assets/icons/icon_120x120.png',
 	'assets/icons/icon_128x128.png',
 	'assets/icons/icon_150x150.png',
 	'assets/icons/icon_152x152.png',
 	'assets/icons/icon_167x167.png',
 	'assets/icons/icon_180x180.png',
 	'assets/icons/icon_192x192.png',
+	'assets/icons/icon_310x150.png',
 	'assets/icons/icon_310x310.png',
 	'assets/icons/icon_512x512.png'
-
-	// Scene Images
-	//'assets/scenes/',
-
-	// Character Images
-	//'assets/characters/',
-
-	// Side Images
-	//'assets/characters/',
-
-	// UI Images
-	//'assets/ui/',
-
-	// Music Audio
-	// 'assets/music/',
-
-	// Voice Audio
-	// 'assets/voice/'
-
-	// Sound Audio
-	// 'assets/sound/'
 ];
 
-self.addEventListener('install', function (event) {
-	event.waitUntil(
-		caches.open(`${name}-v${version}`).then(function (cache) {
+self.addEventListener ('install', (event) => {
+	event.waitUntil (
+		caches.open (`${name}-v${version}`).then ((cache) => {
 			return cache.addAll (files);
 		})
 	);
 });
 
-self.addEventListener('activate', function (event) {
-	event.waitUntil(
-		caches.keys().then(function (keyList) {
-			return Promise.all(keyList.map(function (key) {
+self.addEventListener ('activate', (event) => {
+	event.waitUntil (
+		caches.keys ().then ((keyList) => {
+			return Promise.all (keyList.map ((key) => {
 				if (key !== `${name}-v${version}`) {
 					return caches.delete (key);
 				}
 			}));
 		})
 	);
-
-	return self.clients.claim();
+	return self.clients.claim ();
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener ('fetch', (event) => {
+
 	if (event.request.method !== 'GET') {
 		return;
 	}
 
-	event.respondWith(
-		caches.match(event.request).then(function (cached) {
-			var networked = fetch(event.request)
-				.then(fetchedFromNetwork, unableToResolve)
-				.catch(unableToResolve);
-			return cached || networked;
-
+	event.respondWith (
+		caches.match (event.request).then ((cached) => {
 			function fetchedFromNetwork (response) {
-				var cacheCopy = response.clone();
+				const cacheCopy = response.clone ();
 
-				caches.open(`${name}-v${version}`).then(function add (cache) {
-					cache.put(event.request, cacheCopy);
+				caches.open (`${name}-v${version}`).then (function add (cache) {
+					cache.put (event.request, cacheCopy);
 				});
-
 				return response;
 			}
 
 			function unableToResolve () {
-				return new Response(`
-					<!DOCTYPE html><html lang=en><title>Service Unavailable</title><meta charset=UTF-8><meta content="width=device-width,initial-scale=1"name=viewport><style>html{width:100%;	height:100%}body{width:100%;height:100%;text-align:center;color:#545454;font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Fira Sans","Droid Sans","Helvetica Neue",sans-serif;margin:0}body>div{margin:0 auto;top:50%;left:50%;-webkit-transform:translate(-50%,-50%);-moz-transform:translate(-50%,-50%);-o-transform:translate(-50%,-50%);-ms-transform:translate(-50%,-50%);position:absolute;max-height:100%}h1{font-size:4em;font-weight:lighter}h2{font-size:2em;font-weight:lighter}</style><div><h1>Service Unavailable</h1><h2>Sorry, the server is currently unavailable or under maintenance, try again later.</h2></div>
+				return new Response (`
+					<!DOCTYPE html><html lang=en><title>Bad Request</title><meta charset=UTF-8><meta content="width=device-width,initial-scale=1"name=viewport><style>body,html{width:100%;height:100%}body{text-align:center;color:#545454;margin:0;display:flex;justify-content:center;align-items:center;font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Fira Sans","Droid Sans","Helvetica Neue",sans-serif}h1,h2{font-weight:lighter}h1{font-size:4em}h2{font-size:2em}</style><div><h1>Service Unavailable</h1><h2>Sorry, the server is currently unavailable or under mantainance, try again later.</h2></div>
 				`, {
 					status: 503,
 					statusText: 'Service Unavailable',
-					headers: new Headers({
+					headers: new Headers ({
 						'Content-Type': 'text/html'
 					})
 				});
 			}
+
+			const networked = fetch (event.request)
+				.then (fetchedFromNetwork, unableToResolve)
+				.catch (unableToResolve);
+
+			return cached || networked;
 		})
 	);
 });
