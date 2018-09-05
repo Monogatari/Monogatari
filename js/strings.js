f932128415f411d65995dfcd6575c53ef955ff5a
Ruolin Zheng <ruolinzheng@RuolindeMacBook-Pro.local>
2018-09-05 21:24:12 +0800

    Added simplified Chinese translation in strings.js

--- js/strings.js
+++ js/strings.js
@@ -10,6 +10,7 @@
  * Français - BakaKiller
  * 日本語
  * Nederlands - Natsukeep
+ * 简体中文 - RuolinZheng08
  **/
 
 "use strict";
@@ -325,5 +326,67 @@ const strings = {
 		"Voice": "Stem Volume",
 
 		"Windowed": "Window modus"
-	}
+	},
+
+    "简体中文": {
+        "AdvanceHelp": "按下空格键或点击屏幕以继续。",
+        "Audio": "音效",
+        "AutoPlay": "自动",
+        "AutoPlaySpeed": "自动播放速度",
+
+        "Back": "后退",
+        "BackButton": "后退。",
+
+        "Cancel": "取消",
+        "Close": "关闭",
+        "Confirm": "确定要退出吗？",
+
+        "FullScreen": "全屏显示",
+
+        "Help": "帮助",
+        "Hide": "隐藏",
+        "HideButton": "隐藏文字。",
+
+        "iOSAudioWarning": "iOS暂不支持音效设定。",
+
+        "Language": "语言",
+        "Load": "读取",
+        "LoadAutoSaveSlots": "自动存储的游戏进度",
+        "LoadButton": "显示读取界面。",
+        "Loading": "加载中",
+        "LoadingMessage": "等待素材加载中。",
+        "LoadSlots": "存储的游戏进度",
+        "LocalStorageWarning": "该浏览器暂不支持本地存储功能。",
+
+        "Music": "音乐音量",
+
+        "NoSavedGames": "没有存储的游戏进度。",
+        "NoAutoSavedGames": "没有自动存储的游戏进度。",
+
+        "Overwrite": "覆盖",
+
+        "QuickButtons": "快捷菜单按钮。",
+        "Quit": "退出",
+        "QuitButton": "退出游戏。",
+
+        "Resolution": "分辨率",
+
+        "Save": "存档",
+        "SaveButon": "显示存档界面。",
+        "SaveInSlot": "写入存档槽位",
+        "Settings": "环境设定",
+        "SettingsButton": "显示环境设定界面。",
+        "Show": "显示",
+        "SlotDeletion": "确定要删除这个存档槽位吗？",
+        "SlotOverwrite": "确定要覆盖这个存档槽位吗？",
+        "Sound": "音效音量",
+        "Start": "开始",
+        "Stop": "停止",
+
+        "TextSpeed": "文字显示速度",
+
+        "Voice": "语音音量",
+
+        "Windowed": "窗口"
+    }
 };
