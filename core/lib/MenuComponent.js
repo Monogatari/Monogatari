829f9cde48222ddcae1f96af0ad3d9a6a224f862
Diego Islas Ocampo <dev@hyuchia.com>
2019-07-27 13:45:27 -0500

    Simplify menu button rendering to allow template overwrite

--- core/lib/MenuComponent.js
+++ core/lib/MenuComponent.js
@@ -67,27 +67,34 @@ class MenuComponent extends Component {
 	}
 
 	render () {
-		return this.static.buttons ().map ((button) => this.createButton (button).outerHTML).join (' ');
-	}
-
-	createButton (button) {
-		if (typeof button.element !== 'string') {
-			button.element = 'button';
-		}
-
-		const element = document.createElement (button.element);
-
-		if (typeof button.data === 'object') {
-			for (const key of Object.keys (button.data)) {
-				element.dataset[key] = button.data[key];
+		return this.static.buttons ().map ((button) => {
+			// A user could create a custom element to display all or specific
+			// buttons. If no element was set on the button's definition, we'll
+			// assume it to be a simple button.
+			if (typeof button.element !== 'string') {
+				button.element = 'button';
 			}
-		}
 
-		element.setAttribute ('icon', button.icon);
-		element.setAttribute ('string', button.string);
+			const element = document.createElement (button.element);
 
-		return element;
+			if (typeof button.data === 'object') {
+				for (const key of Object.keys (button.data)) {
+					element.dataset[key] = button.data[key];
+				}
+			}
+
+			element.setAttribute ('icon', button.icon);
+			element.setAttribute ('string', button.string);
+
+			if (button.element === 'button') {
+				element.innerHTML = this.engine.string (button.string);
+			}
+
+			return element.outerHTML;
+		}).join (' ');
 	}
 }
+
+MenuComponent.tag = 'lib-menu-component';
 
 export { MenuComponent };
