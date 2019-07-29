ddf4d4f54eed9e747e8e3240ef9be7acd904764c
Diego Islas Ocampo <dev@hyuchia.com>
2019-07-29 17:04:42 -0500

    Change how errors display objects

--- core/lib/FancyError.js
+++ core/lib/FancyError.js
@@ -59,7 +59,7 @@ export class FancyError {
 			if ($_('[data-error]').isVisible ()) {
 				FancyError.queue.unshift (object);
 			} else {
-				$_ready (() => {
+				if ($_('body').length > 0) {
 					$_('body').prepend (`
 						<div class='fancy-error modal modal--active' data-error='${id}'>
 							<div class='modal__content'>
@@ -88,7 +88,39 @@ export class FancyError {
 						FancyError.pop ();
 					});
 					Prism.highlightAll ();
-				});
+				} else {
+					$_ready (() => {
+						$_('body').prepend (`
+							<div class='fancy-error modal modal--active' data-error='${id}'>
+								<div class='modal__content'>
+									<h2>${title}</h2>
+									<p>${message}</p>
+									<div class='padded  text--left'>
+										<h3>Details</h3>
+										${FancyError.render (props)}
+										<hr class='separator--material'/>
+										<h3>Need More Help?</h3>
+										<div class='error-section'>
+											<p>Remember you can always ask for more help if you need it at:</p>
+											<p><b>Discord</b>: <a href='https://discord.gg/gWSeDTz' target='_blank'>https://discord.gg/gWSeDTz</a></p>
+											<p><b>GitHub</b>: <a href='https://github.com/Monogatari/Monogatari/issues/' target='_blank'>https://github.com/Monogatari/Monogatari/issues/</a></p>
+											<p><b>Twitter</b>: <a href='https://twitter.com/monogatari' target='_blank'>https://twitter.com/monogatari</a></p>
+										</div>
+										<button>Ok</button>
+									</div>
+
+								</div>
+							</div>
+						`);
+
+						$_(`[data-error="${id}"] button`).click (function () {
+							$_(`[data-error="${id}"]`).remove ();
+							FancyError.pop ();
+						});
+						Prism.highlightAll ();
+					});
+				}
+
 			}
 		}
 	}
@@ -99,7 +131,11 @@ export class FancyError {
 			if (typeof props[key] === 'string' || typeof props[key] === 'number') {
 				html += `<p><b>${key}</b>: ${props[key]}</p>`;
 			} else if (props[key] instanceof Array) {
-				html += `<p><b>${key}</b>: ${props[key].toString ().replace (/,/g, ', ')}</p>`;
+				html += `<details open><summary><b>${key}</b>:</summary><ul>`;
+				for (const item of props[key]) {
+					html += `<li>${props[key].toString ().replace (/,/g, ', ')}</li>`;
+				}
+				html += '</ul></details></p>';
 			} else if (props[key] instanceof NodeList) {
 				html += `<p><b>${key}</b>:<br><pre>`;
 				for (const item of props[key]) {
