77317c5236b83f159a08493dd525676a016d5eb2
Diego Islas Ocampo <dev@hyuchia.com>
2019-07-29 18:47:34 -0500

    Add small guide on code placing

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,15 +1,38 @@
-/**
- * ==============================
- * Your Javascript Code Goes Here
- * ==============================
- **/
-
 'use strict';
 /* global Monogatari */
 /* global monogatari */
 
-const { $_ready } = Monogatari;
+/**
+ * =============================================================================
+ * This is the file where you should put all your custom JavaScript code,
+ * depending on what you want to do, there are 3 different places in this file
+ * where you can add code.
+ *
+ * 1. Outside the $_ready function: At this point, the page may not be fully
+ *    loaded yet, however you can interact with Monogatari to register new
+ *    actions, components, labels, characters, etc.
+ *
+ * 2. Inside the $_ready function: At this point, the page has been loaded, and
+ *    you can now interact with the HTML elements on it.
+ *
+ * 3. Inside the init function: At this point, Monogatari has been initialized,
+ *    the event listeners for its inner workings have been registered, assets
+ *    have been preloaded (if enabled) and your game is ready to be played.
+ *
+ * You should always keep the $_ready function as the last thing on this file.
+ * =============================================================================
+ **/
+
+const { $_ready, $_ } = Monogatari;
+
+// 1. Outside the $_ready function:
+
 
 $_ready (() => {
-	monogatari.init ('#monogatari');
+	// 2. Inside the $_ready function:
+
+	monogatari.init ('#monogatari').then (() => {
+		// 3. Inside the init function:
+
+	});
 });
