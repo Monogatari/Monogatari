1b11eb27f501f1d5cc0f473a98407908264c0d8a
Diego Islas Ocampo <dev@hyuchia.com>
2020-09-04 17:44:42 -0500

    Add placeholder particle systems

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -68,6 +68,229 @@ Cypress.Commands.add ('loadTestAssets', function (args) {
 		'theme': 'theme.mp3'
 	});
 
+	this.monogatari.action ('particles').particles ({
+		'snow': {
+			'particles': {
+				'number': {
+					'value': 400,
+					'density': {
+						'enable': true,
+						'value_area': 800
+					}
+				},
+				'color': {
+					'value': '#fff'
+				},
+				'shape': {
+					'type': 'circle',
+					'stroke': {
+						'width': 0,
+						'color': '#000000'
+					},
+					'polygon': {
+						'nb_sides': 5
+					},
+					'image': {
+						'src': 'img\/github.svg',
+						'width': 100,
+						'height': 100
+					}
+				},
+				'opacity': {
+					'value': 0.5,
+					'random': true,
+					'anim': {
+						'enable': false,
+						'speed': 1,
+						'opacity_min': 0.1,
+						'sync': false
+					}
+				},
+				'size': {
+					'value': 10,
+					'random': true,
+					'anim': {
+						'enable': false,
+						'speed': 40,
+						'size_min': 0.1,
+						'sync': false
+					}
+				},
+				'line_linked': {
+					'enable': false,
+					'distance': 500,
+					'color': '#ffffff',
+					'opacity': 0.4,
+					'width': 2
+				},
+				'move': {
+					'enable': true,
+					'speed': 6,
+					'direction': 'bottom',
+					'random': false,
+					'straight': false,
+					'out_mode': 'out',
+					'bounce': false,
+					'attract': {
+						'enable': false,
+						'rotateX': 600,
+						'rotateY': 1200
+					}
+				}
+			},
+			'interactivity': {
+				'detect_on': 'canvas',
+				'events': {
+					'onhover': {
+						'enable': true,
+						'mode': 'bubble'
+					},
+					'onclick': {
+						'enable': true,
+						'mode': 'repulse'
+					},
+					'resize': true
+				},
+				'modes': {
+					'grab': {
+						'distance': 400,
+						'line_linked': {
+							'opacity': 0.5
+						}
+					},
+					'bubble': {
+						'distance': 400,
+						'size': 4,
+						'duration': 0.3,
+						'opacity': 1,
+						'speed': 3
+					},
+					'repulse': {
+						'distance': 200,
+						'duration': 0.4
+					},
+					'push': {
+						'particles_nb': 4
+					},
+					'remove': {
+						'particles_nb': 2
+					}
+				}
+			},
+			'retina_detect': true
+		},
+		'fireflies': {
+			'particles': {
+				'number': {
+					'value': 202,
+					'density': {
+						'enable': true,
+						'value_area': 800
+					}
+				},
+				'color': {
+					'value': '#0bd318'
+				},
+				'shape': {
+					'type': 'circle',
+					'stroke': {
+						'width': 0,
+						'color': '#000000'
+					},
+					'polygon': {
+						'nb_sides': 5
+					},
+					'image': {
+						'src': 'img/github.svg',
+						'width': 100,
+						'height': 100
+					}
+				},
+				'opacity': {
+					'value': 0.9299789953020032,
+					'random': true,
+					'anim': {
+						'enable': true,
+						'speed': 1,
+						'opacity_min': 0,
+						'sync': false
+					}
+				},
+				'size': {
+					'value': 3,
+					'random': true,
+					'anim': {
+						'enable': false,
+						'speed': 4,
+						'size_min': 0.3,
+						'sync': false
+					}
+				},
+				'line_linked': {
+					'enable': false,
+					'distance': 150,
+					'color': '#ffffff',
+					'opacity': 0.4,
+					'width': 1
+				},
+				'move': {
+					'enable': true,
+					'speed': 3.017060304327615,
+					'direction': 'none',
+					'random': true,
+					'straight': false,
+					'out_mode': 'out',
+					'bounce': false,
+					'attract': {
+						'enable': false,
+						'rotateX': 1042.21783956259,
+						'rotateY': 600
+					}
+				}
+			},
+			'interactivity': {
+				'detect_on': 'canvas',
+				'events': {
+					'onhover': {
+						'enable': true,
+						'mode': 'bubble'
+					},
+					'onclick': {
+						'enable': true,
+						'mode': 'repulse'
+					},
+					'resize': true
+				},
+				'modes': {
+					'grab': {
+						'distance': 400,
+						'line_linked': {
+							'opacity': 1
+						}
+					},
+					'bubble': {
+						'distance': 250,
+						'size': 0,
+						'duration': 2,
+						'opacity': 0,
+						'speed': 3
+					},
+					'repulse': {
+						'distance': 400,
+						'duration': 0.4
+					},
+					'push': {
+						'particles_nb': 4
+					},
+					'remove': {
+						'particles_nb': 2
+					}
+				}
+			},
+			'retina_detect': true
+		}
+	});
+
 	this.monogatari.characters ({
 		'y': {
 			name: 'Yui',
