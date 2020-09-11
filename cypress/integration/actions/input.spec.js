a3d2a58b0a9a59c3a5f1f89e3e838cf60461255f
Diego Islas Ocampo <dev@hyuchia.com>
2020-09-10 20:55:39 -0500

    Add timer support for input

--- cypress/integration/actions/input.spec.js
+++ cypress/integration/actions/input.spec.js
@@ -148,4 +148,181 @@ context ('Input', function () {
 
 		cy.get ('[data-content="warning"]').contains ('You must enter a name!');
 	});
+
+	it ('Shows a timer when one is provided', function () {
+		this.monogatari.setting ('TypeAnimation', false);
+		this.monogatari.script ({
+			'Start': [
+				'One',
+				{
+					'Input': {
+						'Text': 'Input',
+						'Validation': function (input) {
+							return input.trim ().length > 0;
+						},
+						'Save': function (input) {
+							this.storage ({
+								player: {
+									name: input
+								}
+							});
+							return true;
+						},
+						'Revert': function () {
+							this.storage ({
+								player: {
+									name: ''
+								}
+							});
+						},
+						'Timer': {
+							// Time in milliseconds
+							time: 5000,
+							// The function to run when the time is over
+							callback: () => {
+								// Get all choices being shown and that are not disabled
+								// const choices = window.monogatari.element ().find ('[data-choice]:not([disabled])');
+
+								// // Pick one of those options randomly
+								// const random = choices.get (window.monogatari.random (0, choices.length - 1));
+
+								// // Fake a click on it
+								// random.click ();
+
+								// Promise friendly!
+								return Promise.resolve ();
+							}
+						},
+						'Warning': 'You must enter a name!'
+					}
+				},
+				'Two',
+			]
+		});
+
+		cy.start ();
+
+		cy.wrap (this.monogatari).invoke ('global', '_InputTimer').should ('equal', null);
+		cy.get ('text-box').contains ('One');
+
+		cy.proceed ();
+
+		cy.get ('text-input').should ('exist');
+		cy.get ('[data-content="message"]').contains ('Input');
+		cy.get ('[data-content="field"]').should ('be.visible');
+		cy.get ('timer-display').should ('be.visible');
+
+		cy.wrap (this.monogatari).invoke ('global', '_InputTimer').should ('not.equal', null);
+
+		cy.get ('[data-content="field"]').type ('My Name');
+
+		cy.get ('[type="submit"]').click ();
+
+		cy.wrap (this.monogatari).invoke ('global', '_InputTimer').should ('equal', null);
+		cy.get ('text-box').contains ('Two');
+
+		cy.rollback ();
+
+		cy.get ('text-input').should ('exist');
+		cy.get ('[data-content="message"]').contains ('Input');
+		cy.get ('[data-content="field"]').should ('be.visible');
+		cy.get ('timer-display').should ('be.visible');
+
+		cy.wrap (this.monogatari).invoke ('global', '_InputTimer').should ('not.equal', null);
+
+		cy.rollback ();
+
+		cy.wrap (this.monogatari).invoke ('global', '_InputTimer').should ('equal', null);
+		cy.get ('text-box').contains ('One');
+
+	});
+
+	it ('Gets filled automatically when timer is done', function () {
+		this.monogatari.setting ('TypeAnimation', false);
+		this.monogatari.script ({
+			'Start': [
+				'One',
+				{
+					'Input': {
+						'Text': 'Input',
+						'Validation': function (input) {
+							return input.trim ().length > 0;
+						},
+						'Save': function (input) {
+							this.storage ({
+								player: {
+									name: input
+								}
+							});
+							return true;
+						},
+						'Revert': function () {
+							this.storage ({
+								player: {
+									name: ''
+								}
+							});
+						},
+						'Timer': {
+							// Time in milliseconds
+							time: 5000,
+							// The function to run when the time is over
+							callback: function () {
+								console.log ('DONE');
+								// Get all choices being shown and that are not disabled
+								const input = this.element ().find ('text-input').get (0);
+
+								input.content ('field').value ('My Name');
+
+								// // Pick one of those options randomly
+								const submit = input.element ().find ('button').get (0);
+
+								// // Fake a click on it
+								submit.click ();
+
+								// Promise friendly!
+								return Promise.resolve ();
+							}
+						},
+						'Warning': 'You must enter a name!'
+					}
+				},
+				'{{player.name}}',
+			]
+		});
+
+		cy.start ();
+
+		cy.wrap (this.monogatari).invoke ('global', '_InputTimer').should ('equal', null);
+		cy.get ('text-box').contains ('One');
+
+		cy.proceed ();
+
+		cy.get ('text-input').should ('exist');
+		cy.get ('[data-content="message"]').contains ('Input');
+		cy.get ('[data-content="field"]').should ('be.visible');
+		cy.get ('timer-display').should ('be.visible');
+
+		cy.wrap (this.monogatari).invoke ('global', '_InputTimer').should ('not.equal', null);
+
+		cy.wait (6000);
+
+		cy.wrap (this.monogatari).invoke ('global', '_InputTimer').should ('equal', null);
+		cy.get ('text-box').contains ('My Name');
+
+		cy.rollback ();
+
+		cy.get ('text-input').should ('exist');
+		cy.get ('[data-content="message"]').contains ('Input');
+		cy.get ('[data-content="field"]').should ('be.visible');
+		cy.get ('timer-display').should ('be.visible');
+
+		cy.wrap (this.monogatari).invoke ('global', '_InputTimer').should ('not.equal', null);
+
+		cy.rollback ();
+
+		cy.wrap (this.monogatari).invoke ('global', '_InputTimer').should ('equal', null);
+		cy.get ('text-box').contains ('One');
+
+	});
 });
--- src/actions/InputModal.js
+++ src/actions/InputModal.js
@@ -3,6 +3,46 @@ import { Util } from '@aegis-framework/artemis';
 
 export class InputModal extends Action {
 
+	static setup () {
+		this.engine.global ('_InputTimer', null);
+
+		this.engine.global ('_input_just_rolled_back', false);
+
+		return Promise.resolve ();
+	}
+
+	static reset () {
+		this.engine.global ('_InputTimer', null);
+
+		this.engine.global ('_input_just_rolled_back', false);
+
+		return Promise.resolve ();
+	}
+
+	static afterRevert () {
+		// When a choice gets reverted, it pushes a `true` value to this global variable.
+		// As soon as it gets reverted, this function is run and it pops the `true` out of
+		// the array, meaning it was just reverted and the choice should be showing on screeen again.
+		if (this.engine.global ('_input_just_rolled_back')) {
+			this.engine.global ('_input_just_rolled_back', false);
+			return Promise.resolve ();
+		}
+
+		// If the player reverts once more while the choice is being shown, then we'll reach this part
+		// and we can clean up any variables we need to.
+		const timer = this.engine.global ('_InputTimer');
+
+		if (timer !== null) {
+			clearTimeout (timer.props.timer);
+			if (timer.parentNode !== null) {
+				timer.element ().remove ();
+			}
+			this.engine.global ('_InputTimer', null);
+		}
+		return Promise.resolve ();
+	}
+
+
 	static matchObject ({ Input }) {
 		return typeof Input !== 'undefined';
 	}
@@ -42,6 +82,10 @@ export class InputModal extends Action {
 		if (typeof this.statement.Options !== 'object' || this.statement.Options === null) {
 			this.statement.Options = [];
 		}
+
+		if (typeof this.statement.Timer !== 'object') {
+			this.statement.Timer = null;
+		}
 	}
 
 	apply () {
@@ -49,7 +93,7 @@ export class InputModal extends Action {
 
 		const input = document.createElement ('text-input');
 
-		const { Text, Warning, Save, Validation, actionString, Class, Type, Options, Default } = this.statement;
+		const { Text, Warning, Save, Validation, actionString, Class, Type, Options, Default, Timer } = this.statement;
 
 		input.setProps ({
 			text: this.engine.replaceVariables (Text),
@@ -61,13 +105,31 @@ export class InputModal extends Action {
 			validate: Validation,
 			actionString,
 			callback: () => {
+				const timer = this.engine.global ('_InputTimer');
+
+				if (timer !== null) {
+					clearTimeout (timer.props.timer);
+					if (timer.parentNode !== null) {
+						timer.element ().remove ();
+					}
+					this.engine.global ('_InputTimer', null);
+				}
+
 				this.engine.global ('block', false);
 				this.engine.proceed ({ userInitiated: true, skip: false, autoPlay: false });
 			},
 			classes: Class.trim ()
 		});
 
+		if (Timer !== null) {
+			const timer_display = document.createElement ('timer-display');
+			timer_display.setProps (Timer);
+			this.engine.global ('_InputTimer', timer_display);
+			this.engine.element ().find ('[data-screen="game"]').prepend (timer_display);
+		}
+
 		this.engine.element ().find ('[data-screen="game"]').append (input);
+
 		return Promise.resolve ();
 	}
 
@@ -75,7 +137,7 @@ export class InputModal extends Action {
 		if (typeof this.statement.Revert === 'function') {
 			return Promise.resolve ();
 		}
-		return Promise.reject ();
+		return Promise.reject ('Input is missing a `Revert` function.');
 	}
 
 	revert () {
@@ -85,6 +147,7 @@ export class InputModal extends Action {
 	}
 
 	didRevert () {
+		this.engine.global ('_input_just_rolled_back', true);
 		return Promise.resolve ({ advance: false, step: true });
 	}
 }
